c2f8b4f405c8c41e09d9d85cbcb849fe0f8b376a
Vaibhav Gitte <vrgitte9075@gmail.com>
2026-01-05 13:37:36 +0530

    Add files via upload

---
+++ str_task.py
@@ -0,0 +1,56 @@
+
+s1="Hellow World"
+print(s1.upper())
+
+s2="PYTHON PROGRAMMING"
+print(s2.lower())
+
+s3='python is fun'
+print(s3.capitalize())
+
+s4='Data Science'
+print(len(s4))
+
+s5='This is bad idea '
+print(s5.replace('bad','good'))
+
+s6="Hellow World"
+print(s6.index('o'))
+
+print("Count of l=",s6.count('l'))
+
+s7='python123'
+print(s7.isalpha())
+
+s8='123456'
+print(s8.isdigit())
+
+s9='apple,banana,cherry'
+print(s9.split())
+
+l=['python','is','awesome']
+print(" ".join(l))
+
+s11=' Hellow World '
+print(s11.strip())
+
+print(s6.startswith('H'))
+
+print(s6.endswith('World'))
+
+s12='abcdef'
+print(s12[::-1])
+
+print(s6[7::])
+
+s13='python is fun'
+print(s13.replace(' ',"-"))
+
+s14='123'
+a=int(s14)
+print(a)
+print(type(a))
+
+b=456
+s=str(b)
+print(s)
